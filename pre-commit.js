59730253ed1a61f6fe729a9b6f9aba101e2c2ef2
AndriiKot <AndriiKot@users.noreply.github.com>
2025-05-03 07:29:49 +0200

    'Responsive Web Design' **refactore: pre-commit.js testing 1

--- pre-commit.js
+++ pre-commit.js
@@ -3,7 +3,7 @@
 const path = require("path");
 const { execSync } = require("child_process");
 
-const undoChanges = []
+const undoChanges = [];
 
 // const test1 = execSync("git diff --name-only ", { encoding: "utf-8" })
 // const test2 = execSync("git diff --cached --name-only", { encoding: "utf-8" })
@@ -11,79 +11,37 @@ const undoChanges = []
 // console.log(`Test1: 'git diff --name-only': ${test1}`);
 // console.log(`Test2: 'git diff --cached --name-only': ${test2}`);
 
+const test3 = execSync("git ls-files --others --exclude-standard", {
+  encoding: "utf-8",
+});
 
-const test3 = execSync("git ls-files --others --exclude-standard", { encoding: "utf-8" });
-
-const arrFolders = test3.split('\n');
+const arrFolders = test3.split("\n");
 console.dir(arrFolders);
 
-arrFolders.forEach((el,i, arr) => { 
- if (el.includes('Building_a_Magazine')) {
-    undoChanges.push(el)
+arrFolders.forEach((el, i, arr) => {
+  if (el.includes("Building_a_Magazine")) {
+    undoChanges.push(el);
   }
-})
+});
 
 console.log(undoChanges);
-undoChanges.length = 0
+undoChanges.length = 0;
 console.log(undoChanges);
 
-const statuses = [
- '??', 'A',
-]
+const statuses = ["??", "A"];
 
 const status = execSync("git status --short", { encoding: "utf-8" });
 
-console.log(status.split('\n'));
+console.log(status.split("\n"));
 
 // execSync(`git clean -f ${undoChanges.join(' ')}`)   // if you '??'
 // execSync('git restore ${}`)                         // if you 'AD', 'AM', 'M', 'D'
 // execSync(`git restore --staged ${}`)                // if you 'A'
 
+config = ["Building_a_Magazine"];
 
-
-
-/*
-const util = require("node:util");
-const exec = util.promisify(require("node:child_process").exec);
-
-const projectRoot = path.join(__dirname, "..");
-
-const scriptPath = path.join(
-  projectRoot,
-  "automation",
-  "ManagerREADMES",
-  "manager_readmes.js"
-);
-
-const diffedFiles2 = execSync("git diff --name-only", { encoding: "utf-8" })
-  .trim()
-  .split("\n");
-const stagedFiles2 = execSync("git diff --staged --name-only", {
-  encoding: "utf-8",
-})
-  .trim()
-  .split("\n");
-const newFiles2 = execSync("git ls-files --others --exclude-standard", {
-  encoding: "utf-8",
-})
-  .trim()
-  .split("\n");
-
-const relevantFiles = [...diffedFiles2, ...stagedFiles2, ...newFiles2].filter(
-  (file) =>
-    ["styles.css", "index.html", "script.js"].includes(path.basename(file))
-);
-
-
-(async () => {
-  if (relevantFiles.length > 0) {
-    require(scriptPath);
-  } else {
-    console.log("No relevant changes detected. Skipping script execution.");
-  }
-})();
-
-setTimeout(() => {
-   execSync("git add .", { stdio: "inherit" });  
-}, 10000);
-  */
+config.forEach((el, i, arr) => {
+  execSync(`git restore --staged ${el}`);
+  execSync(`git restore ${el}`);
+  execSync(`git clean -f ${el}`);
+});
