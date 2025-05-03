2efb171e6d53b7c09ddb3487b556690a3d4508c1
AndriiKot <AndriiKot@users.noreply.github.com>
2025-05-03 07:47:08 +0200

    'Responsive Web Design' **refactore: pre-commit__v_1_0_0

--- pre-commit.js
+++ pre-commit.js
@@ -3,44 +3,15 @@
 const path = require("path");
 const { execSync } = require("child_process");
 
-const undoChanges = [];
-
-// const test1 = execSync("git diff --name-only ", { encoding: "utf-8" })
-// const test2 = execSync("git diff --cached --name-only", { encoding: "utf-8" })
-
-// console.log(`Test1: 'git diff --name-only': ${test1}`);
-// console.log(`Test2: 'git diff --cached --name-only': ${test2}`);
-
-const test3 = execSync("git ls-files --others --exclude-standard", {
-  encoding: "utf-8",
-});
-
-const arrFolders = test3.split("\n");
-console.dir(arrFolders);
-
-arrFolders.forEach((el, i, arr) => {
-  if (el.includes("Building_a_Magazine")) {
-    undoChanges.push(el);
-  }
-});
-
-console.log(undoChanges);
-undoChanges.length = 0;
-console.log(undoChanges);
-
-const statuses = ["??", "A"];
-
-const status = execSync("git status --short", { encoding: "utf-8" });
-
-console.log(status.split("\n"));
 
 // execSync(`git clean -f ${undoChanges.join(' ')}`)   // if you '??'
 // execSync('git restore ${}`)                         // if you 'AD', 'AM', 'M', 'D'
 // execSync(`git restore --staged ${}`)                // if you 'A'
 
-config = ["Building_a_Magazine"];
+configCancelChangesFolders = ["Building_a_Magazine"];
 
 config.forEach((el, i, arr) => {
+  execSync(`git restore ${el}`);
   execSync(`git restore --staged ${el}`);
   execSync(`git restore ${el}`);
   execSync(`git clean -f ${el}`);
